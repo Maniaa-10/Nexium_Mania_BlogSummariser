194658bf909782088c9a033fbe9788cd384b4362
Mania Shakeel <maniashakeel10@gmail.com>
2025-07-16 11:24:29 +0500

    Tooltip icon with message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@
 import { useState } from "react"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
+import { Tooltip, TooltipTrigger, TooltipContent, TooltipProvider } from "@/components/ui/tooltip"
+import { Info } from "lucide-react"
 
 export default function HomePage() 
 {
@@ -102,16 +104,30 @@ export default function HomePage()
             </div>
           ) : (
             <div>
+                            <TooltipProvider>
+  <Tooltip>
+    <TooltipTrigger asChild>
+      <Info className="w-4 h-4 ml-2 cursor-pointer" />
+    </TooltipTrigger>
+    <TooltipContent side="top" className="text-xs">
+              <div className="flex flex-col items-center" >
+                <p className="text-xs text-white text-centre italic mt-1 ">
+                ⚠️Only a few words translated via hardcoded limited JS dictionary</p>
+                <p className="text-xs text-white text-centre mb-1">
+                (یہ ترجمہ محدود جاوا اسکرپٹ لغت پر مبنی ہے  )  </p>
+              </div>
+    </TooltipContent>
+  </Tooltip>
+</TooltipProvider>
               <h2 className="font-semibold text-right mb-2">:اردو ترجمہ</h2>
               <p className="text-sm text-gray-800 text-right whitespace-pre-wrap">{urdu}</p>
-              <p className="text-sm text-gray-700 text-centre italic mt-4 px-15">
-              ⚠️Only a few words translated via hardcoded limited JS dictionary</p>
-              <p className="text-xs text-gray-600 text-right mt-1 px-38">
-              (یہ ترجمہ محدود جاوا اسکرپٹ لغت پر مبنی ہے  )  </p>
+
+
             </div>
       )}
       </div>
       )}
+
 
       <button
         onClick={() => 
@@ -121,7 +137,7 @@ export default function HomePage()
           setUrl("")
         }
         }
-        className="fixed bottom-4 right-4 bg-indigo-400 hover:bg-pink-400 text-white rounded-full px-4 py-2 shadow-lg transition-all"
+        className="font-semibold fixed bottom-4 right-4 bg-indigo-400 hover:bg-pink-400 text-white rounded-full px-4 py-2 shadow-lg transition-all"
         >
         Home
       </button>
